8efe041170e7683220bbd26545fca740785e526c
Tiankai <tiankai@umich.edu>
2021-12-03 19:12:18 -0500

    complete unit test

--- tests/testthat/test-lm2.R
+++ tests/testthat/test-lm2.R
@@ -3,23 +3,27 @@ test_that("lm2: test default na.handle & res_display work", {
 })
 
 test_that("lm2: test na.fail na.action works", {
-  expect_error(lm2(formula = Rape ~ Murder+Assault+UrbanPop, data = USArrests, na.handle = "na.fail"))
+  expect_error(lm2(formula = Temp ~ Wind + Solar.R, data = airquality, na.handle = "na.fail"))
 })
 
 test_that("lm2: missing.N Correct", {
-  expect_equal(lm2(formula = Rape ~ Murder+Assault+UrbanPop, data = USArrests)$missing.N, 0)
+  expect_equal(lm2(formula = Rape ~ Murder+Assault+UrbanPop, data = USArrests, res_display = F)$missing.N, 0)
 })
 
 test_that("lm2: Residual Degrees of Freedom Correct", {
-  expect_equal(lm2(formula = Rape ~ Murder+Assault+UrbanPop, data = USArrests)$df, 46)
+  expect_equal(lm2(formula = Rape ~ Murder+Assault+UrbanPop, data = USArrests, res_display = F)$df, 46)
 })
 
-test_that("lm2: Residuals Match LM output", {
-  expect_equal(lm2(formula = Rape ~ Murder+Assault+UrbanPop, data = USArrests)$residuals, lm(formula = Rape ~ Murder+Assault+UrbanPop, data = USArrests)$residuals)
+test_that("lm2: Coefficent Match LM's output", {
+  expect_equal(lm2(formula = Rape ~ Murder+Assault+UrbanPop, data = USArrests, res_display = F)$coefficients, lm(formula = Rape ~ Murder+Assault+UrbanPop, data = USArrests)$coefficients)
 })
 
-test_that("lm2: Coefficent Match LM output", {
-  expect_equal(lm2(formula = Rape ~ Murder+Assault+UrbanPop, data = USArrests)$coefficients, lm(formula = Rape ~ Murder+Assault+UrbanPop, data = USArrests)$coefficients)
+test_that("lm2: Residuals Match LM's output", {
+  expect_equal(lm2(formula = Rape ~ Murder+Assault+UrbanPop, data = USArrests, res_display = F)$residuals, lm(formula = Rape ~ Murder+Assault+UrbanPop, data = USArrests)$residuals)
+})
+
+test_that("lm2:Fitted values Match LM's output", {
+  expect_equal(lm2(formula = Rape ~ Murder+Assault+UrbanPop, data = USArrests, res_display = F)$fitted.values, lm(formula = Rape ~ Murder+Assault+UrbanPop, data = USArrests)$fitted.values)
 })
 
 test_that("lm2: Check that input for formula argument is required", {
@@ -29,7 +33,3 @@ test_that("lm2: Check that input for formula argument is required", {
 test_that("lm2: Check that input for data argument is required", {
   expect_error(lm2(formula = Rape ~ Murder+Assault+UrbanPop))
 })
-
-
-
-
---
+++ tests/testthat/test-summary_lm2.R
@@ -0,0 +1,32 @@
+test_that("summary_lm2: Residuals Correct", {
+  expect_equal(summary_lm2(lm2(formula = Rape ~ Murder+Assault+UrbanPop, data = USArrests, res_display = F))$residuals,
+               summary(lm(formula = Rape ~ Murder+Assault+UrbanPop, data = USArrests))$residuals)
+})
+
+test_that("summary_lm2: Covariance Matrix Correct", {
+  expect_equal(as.matrix(summary_lm2(lm2(formula = Rape ~ Murder+Assault+UrbanPop, data = USArrests, res_display = F))$coefficients),
+               as.matrix(summary(lm(formula = Rape ~ Murder+Assault+UrbanPop, data = USArrests))$coefficients))
+})
+
+test_that("summary_lm2: R square Correct", {
+  expect_equal(as.matrix(summary_lm2(lm2(formula = Rape ~ Murder+Assault+UrbanPop, data = USArrests, res_display = F))$r.squared),
+               as.matrix(summary(lm(formula = Rape ~ Murder+Assault+UrbanPop, data = USArrests))$r.squared))
+})
+
+
+test_that("summary_lm2: Adjusted R Square Correct", {
+  expect_equal(summary_lm2(lm2(formula = Rape ~ Murder+Assault+UrbanPop, data = USArrests, res_display = F))$adj.r.squared,
+               summary(lm(formula = Rape ~ Murder+Assault+UrbanPop, data = USArrests))$adj.r.squared)
+})
+
+
+test_that("summary_lm2: Fstatistic Correct", {
+  expect_equal(summary_lm2(lm2(formula = Rape ~ Murder+Assault+UrbanPop, data = USArrests, res_display = F))$fstatistic,
+               summary(lm(formula = Rape ~ Murder+Assault+UrbanPop, data = USArrests))$fstatistic)
+})
+
+
+test_that("summary_lm2: Unscaled Covariance Matrix Correct", {
+  expect_equal(summary_lm2(lm2(formula = Rape ~ Murder+Assault+UrbanPop, data = USArrests, res_display = F))$cov.unscaled,
+               summary(lm(formula = Rape ~ Murder+Assault+UrbanPop, data = USArrests))$cov.unscaled)
+})
