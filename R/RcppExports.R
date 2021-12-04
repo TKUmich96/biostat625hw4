b61d19b1553a07ce4dd8f48475b2535786685085
Tiankai <tiankai@umich.edu>
2021-12-03 20:26:13 -0500

    go blue

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -5,19 +5,3 @@ get_Cal_val <- function(x, y) {
     .Call(`_lm2Wsmry_get_Cal_val`, x, y)
 }
 
-rcpparma_hello_world <- function() {
-    .Call(`_lm2Wsmry_rcpparma_hello_world`)
-}
-
-rcpparma_outerproduct <- function(x) {
-    .Call(`_lm2Wsmry_rcpparma_outerproduct`, x)
-}
-
-rcpparma_innerproduct <- function(x) {
-    .Call(`_lm2Wsmry_rcpparma_innerproduct`, x)
-}
-
-rcpparma_bothproducts <- function(x) {
-    .Call(`_lm2Wsmry_rcpparma_bothproducts`, x)
-}
-
--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -18,56 +18,9 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
-// rcpparma_hello_world
-arma::mat rcpparma_hello_world();
-RcppExport SEXP _lm2Wsmry_rcpparma_hello_world() {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    rcpp_result_gen = Rcpp::wrap(rcpparma_hello_world());
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcpparma_outerproduct
-arma::mat rcpparma_outerproduct(const arma::colvec& x);
-RcppExport SEXP _lm2Wsmry_rcpparma_outerproduct(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_outerproduct(x));
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcpparma_innerproduct
-double rcpparma_innerproduct(const arma::colvec& x);
-RcppExport SEXP _lm2Wsmry_rcpparma_innerproduct(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_innerproduct(x));
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcpparma_bothproducts
-Rcpp::List rcpparma_bothproducts(const arma::colvec& x);
-RcppExport SEXP _lm2Wsmry_rcpparma_bothproducts(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_bothproducts(x));
-    return rcpp_result_gen;
-END_RCPP
-}
 
 static const R_CallMethodDef CallEntries[] = {
     {"_lm2Wsmry_get_Cal_val", (DL_FUNC) &_lm2Wsmry_get_Cal_val, 2},
-    {"_lm2Wsmry_rcpparma_hello_world", (DL_FUNC) &_lm2Wsmry_rcpparma_hello_world, 0},
-    {"_lm2Wsmry_rcpparma_outerproduct", (DL_FUNC) &_lm2Wsmry_rcpparma_outerproduct, 1},
-    {"_lm2Wsmry_rcpparma_innerproduct", (DL_FUNC) &_lm2Wsmry_rcpparma_innerproduct, 1},
-    {"_lm2Wsmry_rcpparma_bothproducts", (DL_FUNC) &_lm2Wsmry_rcpparma_bothproducts, 1},
     {NULL, NULL, 0}
 };
 
